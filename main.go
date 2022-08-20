5783f590ec2e958995b4113bd8ba5ef522f6e0bc
RossoDiablo <makarov.daniil@yandex.ru>
2022-08-20 17:02:51 +0300

    new algo

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 	"strings"
 )
 
-func main() {
+func Wordcount() {
 	rdr := bufio.NewReader(os.Stdin)
 	str,_ := rdr.ReadString('\n')
 	arr := strings.Split(str," ")
 	fmt.Print(len(arr))
+}
+
+func main() {
+	Wordcount()
 }
